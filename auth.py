01f5a5db374c0f4288bb0df6758fe89c99e278bf
sturt <stuart0laing@gmail.com>
2019-12-18 12:06:12 +0000

    Added Handling of full pcaps and Dynamically finding essid

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 import dpkt
 
 
-def pretty(data):
+def __pretty(data):
     output_string = ""
     for thing in data:
         if len(hex(thing)[2:]) == 1:
@@ -24,11 +24,6 @@ class EAPOL:
         self.key_mic = ieee_packet.data[89:105]
         self.key_nonce = ieee_packet.data[25:57]
         self.eapol = ieee_packet.data[8:89] + (b"\x00" * 16) + ieee_packet.data[105:]
-
-        #if msg_num == 2:
-        #    print(pretty(ieee_packet.data[8:]))
-        #    print(pretty(self.eapol))
-        #    print("""0103007502010a00000000000000000001d4ef6125ff9344e00e633e8db71f754a4a634ca572d9b6631f8a510179049480000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001630140100000fac040100000fac040100000fac020000""")
 
     def __repr__(self):
         output_string = "EAPOL("
--- convert.py
+++ convert.py
@@ -1,17 +1,50 @@
 import dpkt
+import sys
+
 import auth
 
-"""
-trash = open("output", "wb")
-trash.write(b"\x48\x43\x50\x58")
-trash.close()
-"""
 
 # TODO
-# Have it dynamically find the ESSID
+# ########## Have it dynamically find the ESSID
+# Proper Command Line Arguments
+# ########## Have it work with files that contain more than just eapol packets
+# Have it work with files with multiple 4 way handshakes
+# Allow specification of different message pairs
+# Allow specification of specific eapol handshakes
+# Handle WPA 1
+# Verify given arguments
 
-# pcap_path = "Demo-Files/Demo-Pcap-1.pcap"
-pcap_path = "Demo-Files/Demo-Pcap-Clean-1.pcap"
+def verify_eapol_packet(packet_buffer):
+    try:
+        _ = dpkt.ieee80211.IEEE80211(buffer)
+    except dpkt.dpkt.UnpackError:
+        return False
+
+    if buffer[0] != 0x88:
+        return False
+
+    if buffer[32:34] != b"\x88\x8e":
+        return False
+
+    return True
+
+
+def find_essid(packet_buffer):
+    if packet_buffer[0] != 0x50:
+        return "", 0
+    ieee_packet = dpkt.ieee80211.IEEE80211(packet_buffer).data
+
+    length = ieee_packet[1]
+    name = ieee_packet[2:length + 2]
+
+    return name, length
+
+
+# pcap_path = "Demo-Files/Demo-Pcap-Clean-1.pcap"
+if len(sys.argv) == 2:
+    pcap_path = sys.argv[1]
+else:
+    pcap_path = "Demo-Files/Demo-Pcap-1.pcap"
 
 pcap_file = open(pcap_path, "rb")
 contents = dpkt.pcap.Reader(pcap_file)
@@ -21,44 +54,42 @@ msg_num = 1
 eapol_packets = []
 
 for timestamp, buffer in contents:
-    # print(repr(buffer))
-    eapol_packets.append(auth.EAPOL(buffer, msg_num))
-    msg_num += 1
+    if verify_eapol_packet(buffer):
+        eapol_packets.append(auth.EAPOL(buffer, msg_num))
+        msg_num += 1
+    else:
+        temp_essid, temp_essid_length = find_essid(buffer)
+        if temp_essid:
+            essid = temp_essid
+            essid_length = temp_essid_length
 
-file_signature = b"\x48\x43\x50\x58"
-version = b"\x04\x00\x00\x00"
-message_pair = b"\x00"
-essid_length = b"\x06"
-essid = b"\x57\x6f\x70\x57\x6f\x70" + (b"\x00" * 26)
+try:
+    essid
+    essid_length
+except NameError:
+    print("No Essid Found")
+    exit()
 
-key_ver = b"\x02"
-key_mic = eapol_packets[1].key_mic
+if len(eapol_packets) != 4:
+    print("ERR")
+    exit()
 
-ap_mac = eapol_packets[0].src_mac
-ap_nonce = eapol_packets[0].key_nonce
-
-client_mac = eapol_packets[1].src_mac
-client_nonce = eapol_packets[1].key_nonce
-
-eapol = eapol_packets[1].eapol
-eapol_length = chr(len(eapol)).encode() + b"\x00"
-
-print(len(eapol))
-excess_zeros = b"\x00" * (256 - len(eapol))
+write_contents = {"file_signature": b"\x48\x43\x50\x58",
+                  "version": b"\x04\x00\x00\x00",
+                  "message_pair": b"\x00",
+                  "essid_length": chr(essid_length).encode(),
+                  "essid": essid + (b"\x00" * (32 - essid_length)),
+                  "key_ver": b"\x02",
+                  "key_mic": eapol_packets[1].key_mic,
+                  "ap_mac": eapol_packets[0].src_mac,
+                  "ap_nonce": eapol_packets[0].key_nonce,
+                  "client_mac": eapol_packets[1].src_mac,
+                  "client_nonce": eapol_packets[1].key_nonce,
+                  "eapol_length": chr(len(eapol_packets[1].eapol)).encode() + b"\x00",
+                  "eapol": eapol_packets[1].eapol,
+                  "buffer_zeros": b"\x00" * (256 - len(eapol_packets[1].eapol))}
 
 output_file = open("result_file.hccapx", "wb")
-output_file.write(file_signature)
-output_file.write(version)
-output_file.write(message_pair)
-output_file.write(essid_length)
-output_file.write(essid)
-output_file.write(key_ver)
-output_file.write(key_mic)
-output_file.write(ap_mac)
-output_file.write(ap_nonce)
-output_file.write(client_mac)
-output_file.write(client_nonce)
-output_file.write(eapol_length)
-output_file.write(eapol)
-output_file.write(excess_zeros)
+for value in write_contents.values():
+    output_file.write(value)
 output_file.close()
