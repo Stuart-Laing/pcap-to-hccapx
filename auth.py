4778ad2acaa0f4fb9bbc35e290c69352034924ac
sturt <stuart0laing@gmail.com>
2019-12-18 16:33:25 +0000

    Added proper argument passing

--- auth.py
+++ auth.py
@@ -11,8 +11,34 @@ def __pretty(data):
     return output_string
 
 
+class Handshake:
+    def __init__(self):
+        self.m1 = EAPOL
+        self.m2 = EAPOL
+        self.m3 = EAPOL
+        self.m4 = EAPOL
+        self.message_num = 0
+
+    def add_eapol_packet(self, data, msg_num):
+        if msg_num == 1:
+            self.m1 = EAPOL(data)
+            self.message_num += 1
+
+        elif msg_num == 2:
+            self.m2 = EAPOL(data)
+            self.message_num += 1
+
+        elif msg_num == 3:
+            self.m3 = EAPOL(data)
+            self.message_num += 1
+
+        elif msg_num == 4:
+            self.m4 = EAPOL(data)
+            self.message_num += 1
+
+
 class EAPOL:
-    def __init__(self, data, msg_num):
+    def __init__(self, data):
 
         self.src_mac = data[10:16]
         self.dst_mac = data[4:10]
@@ -20,7 +46,7 @@ class EAPOL:
         ieee_packet = dpkt.ieee80211.IEEE80211(data)
 
         self.key_descriptor_type = ieee_packet.data[12:13]
-        self.message_number = msg_num
+        # self.message_number = msg_num
         self.key_mic = ieee_packet.data[89:105]
         self.key_nonce = ieee_packet.data[25:57]
         self.eapol = ieee_packet.data[8:89] + (b"\x00" * 16) + ieee_packet.data[105:]
@@ -30,7 +56,7 @@ class EAPOL:
         output_string += f"src_mac={self.src_mac}, "
         output_string += f"dst_mac={self.dst_mac}, "
         output_string += f"key_descriptor_type={self.key_descriptor_type}, "
-        output_string += f"message_number={self.message_number}, "
+        # output_string += f"message_number={self.message_number}, "
         output_string += f"key_mic={self.key_mic}, "
         output_string += f"key_nonce={self.key_nonce}, "
         output_string += f"eapol={self.eapol})"
--- convert.py
+++ convert.py
@@ -1,22 +1,53 @@
 import dpkt
 import sys
+import argcurse
 
 import auth
-
 
 # TODO
 # ########## Have it dynamically find the ESSID
 # Proper Command Line Arguments
 # ########## Have it work with files that contain more than just eapol packets
-# Have it work with files with multiple 4 way handshakes
+# ########## Have it work with files with multiple 4 way handshakes
 # Allow specification of different message pairs
 # Allow specification of specific eapol handshakes
 # Handle WPA 1
 # Verify given arguments
 
+
+HELP_MSG = """Usage: convert.py [options] -i input_zip_file
+
+Options:
+ |                 Flags              |          Description          | Required |      Default     |        
+ +------------------------------------+-------------------------------+----------+------------------+
+ |  -i --input-pcap <file path>       | Input pcap file path          | Yes      | None             |
+ |  -o --output-file <file path>      | Output hccapx file path       | No       | ./results.hccapx |
+ |  -e --essid <essid>                | ESSID of the ap               | No       | None             |
+ |  -mp --message-pair <message pair> | Message pair used in the file | No       | 0                |
+ |  -ap --access-mac <mac adr>        | Mac address of access point   | No       | None             |
+ |  -cl --client-mac <mac adr>        | Mac address of client         | No       | None             |
+ |  -h --help                         | Prints this message
+ """
+
+DEFAULT_MSG = """Usage: convert.py [options] -i input_zip_file
+Try -h or --help for more info."""
+
+arg_handler = argcurse.Handler("-h", "--help", HELP_MSG)
+arg_handler.add_default(DEFAULT_MSG)
+
+arg_handler.add_flag("-i", "--input-pcap", has_content=True, required=True)
+arg_handler.add_flag("-o", "--output-file", has_content=True)
+arg_handler.add_flag("-e", "--essid", has_content=True)
+arg_handler.add_flag("-mp", "--message-pair", has_content=True)
+arg_handler.add_flag("-ap", "--access-mac", has_content=True)
+arg_handler.add_flag("-cl", "--client-mac", has_content=True)
+
+arg_handler.compile()
+
+
 def verify_eapol_packet(packet_buffer):
     try:
-        _ = dpkt.ieee80211.IEEE80211(buffer)
+        _ = dpkt.ieee80211.IEEE80211(packet_buffer)
     except dpkt.dpkt.UnpackError:
         return False
 
@@ -40,23 +71,34 @@ def find_essid(packet_buffer):
     return name, length
 
 
-# pcap_path = "Demo-Files/Demo-Pcap-Clean-1.pcap"
-if len(sys.argv) == 2:
-    pcap_path = sys.argv[1]
-else:
-    pcap_path = "Demo-Files/Demo-Pcap-1.pcap"
+pcap_path = arg_handler.results["-i"]
 
 pcap_file = open(pcap_path, "rb")
 contents = dpkt.pcap.Reader(pcap_file)
 
+handshakes = []
+
 msg_num = 1
-
-eapol_packets = []
-
+handshake_index = 0
 for timestamp, buffer in contents:
     if verify_eapol_packet(buffer):
-        eapol_packets.append(auth.EAPOL(buffer, msg_num))
-        msg_num += 1
+        if msg_num == 1:
+            handshakes.append(auth.Handshake())
+            handshakes[handshake_index].add_eapol_packet(buffer, msg_num)
+
+            msg_num += 1
+
+        elif msg_num == 2 or msg_num == 3:
+            handshakes[handshake_index].add_eapol_packet(buffer, msg_num)
+
+            msg_num += 1
+
+        elif msg_num == 4:
+            handshakes[handshake_index].add_eapol_packet(buffer, msg_num)
+
+            msg_num = 1
+            handshake_index += 1
+
     else:
         temp_essid, temp_essid_length = find_essid(buffer)
         if temp_essid:
@@ -70,7 +112,7 @@ except NameError:
     print("No Essid Found")
     exit()
 
-if len(eapol_packets) != 4:
+if handshakes[0].message_num != 4:
     print("ERR")
     exit()
 
@@ -80,16 +122,22 @@ write_contents = {"file_signature": b"\x48\x43\x50\x58",
                   "essid_length": chr(essid_length).encode(),
                   "essid": essid + (b"\x00" * (32 - essid_length)),
                   "key_ver": b"\x02",
-                  "key_mic": eapol_packets[1].key_mic,
-                  "ap_mac": eapol_packets[0].src_mac,
-                  "ap_nonce": eapol_packets[0].key_nonce,
-                  "client_mac": eapol_packets[1].src_mac,
-                  "client_nonce": eapol_packets[1].key_nonce,
-                  "eapol_length": chr(len(eapol_packets[1].eapol)).encode() + b"\x00",
-                  "eapol": eapol_packets[1].eapol,
-                  "buffer_zeros": b"\x00" * (256 - len(eapol_packets[1].eapol))}
+                  "key_mic": handshakes[0].m2.key_mic,
+                  "ap_mac": handshakes[0].m1.src_mac,
+                  "ap_nonce": handshakes[0].m1.key_nonce,
+                  "client_mac": handshakes[0].m2.src_mac,
+                  "client_nonce": handshakes[0].m2.key_nonce,
+                  "eapol_length": chr(len(handshakes[0].m2.eapol)).encode() + b"\x00",
+                  "eapol": handshakes[0].m2.eapol,
+                  "buffer_zeros": b"\x00" * (256 - len(handshakes[0].m2.eapol))}
 
-output_file = open("result_file.hccapx", "wb")
+if arg_handler.results["-o"]:
+    output_file_path = arg_handler.results["-o"]
+else:
+    output_file_path = "results.hccapx"
+
+output_file = open(output_file_path, "wb")
+
 for value in write_contents.values():
     output_file.write(value)
 output_file.close()
